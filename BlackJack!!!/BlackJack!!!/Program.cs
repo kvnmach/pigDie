d2ab77481b9f265f00f15952de003b7541cf8103
kvnmach <kvnmach@gmail.com>
2016-03-03 13:48:02 -0600

    added title

---
+++ BlackJack!!!/BlackJack!!!/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BlackJack!");
+            Console.WriteLine("INSTRUCTIONS: Dealer STAYS on  16, Hit 21 You WIN!, Over 21 you LOSE!");
+            Console.WriteLine("Press any key to continue");
+            Console.WriteLine( DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
