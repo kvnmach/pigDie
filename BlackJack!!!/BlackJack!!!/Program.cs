ee534c0238fd0e319fb4bcc8b9d361f8b826819c
kvnmach <kvnmach@gmail.com>
2016-03-03 19:49:40 -0600

    new cards class built

--- BlackJack!!!/BlackJack!!!/Program.cs
+++ BlackJack!!!/BlackJack!!!/Program.cs
@@ -11,8 +11,44 @@ namespace BlackJack___
         static void Main(string[] args)
         {
             TitlePage();
+            int i = 21;
+            while (true)
+            {
+               
 
-            //
+                }
+                  
+                    {
+                    
+                 
+                    
+                    
+            }
+
+            
+
+
+            //SUIT . RANK NEEDS TO GO TOGETHER
+            //shuffle deck
+            //BUILD A CLASS
+            //draw cards to players 2 each
+            //BUILD IF ELSE STATEMENT TO SEE IF PLAYERS WANT TO HIT OR HOLD
+            //hits until desired amount
+            //HAVE TO RECOGNIZE A SCORE
+            //+=
+            //IF ELSE STATEMENT += EQUALS == THEN 
+            //BUILD IF ELSE STATEMENT IF IT GOES OVER 21 FOR BOTH PLAYERS (BUSTS); IF LESS THAN 16 DRAW FOR DEALER; IF => 16  THEN HOLD FOR DEALER
+            //for each statement to generate READLINE 
+            //draw cards to dealer 2 each 1 hidden
+            //hits until it goes to 16
+            //LOOP
+            //dealer stays on 16 
+            //21 wins
+            // >21 busts
+            //52 cards maxed
+            //KQJ = 10
+            //A = 11
+            
         }
 
         private static void TitlePage()
---
+++ BlackJack!!!/BlackJack!!!/myDeck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack___
+{
+    class DeckofCard
+    {
+        public enum Suit
+            {
+                Hearts,
+                Clubs,
+                Diamonds,
+                Spades
+            }
+
+        public enum Rank
+
+            {
+                Two = 2,
+                Three,
+                Four,
+                Five,
+                Six,
+                Seven,
+                Eight,
+                Nine,
+                Ten,
+                Jack = 10,
+                Queen = 10,
+                King = 10,
+                Ace,
+            }
+           
+        //switch//
+
+    }
+}
